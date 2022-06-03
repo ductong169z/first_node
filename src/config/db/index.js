66d8addd466f544225b75845a318328c13c01aa4
Nguyễn Đức Tông <ductong@miennam24h.vn>
2022-06-04 00:00:39 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/push_notification_dev", {
+        await mongoose.connect("mongodb+srv://tonghgaz12:DucTong123@cluster0.w4zpqvu.mongodb.net/?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
@@ -10,6 +10,25 @@ async function connect() {
     } catch (error) {
         console.log("error", error);
     }
+    // const { MongoClient, ServerApiVersion } = require("mongodb");
+    // const uri =
+    //     "mongodb+srv://tonghgaz12:DucTong123@@cluster0.w4zpqvu.mongodb.net/?retryWrites=true&w=majority";
+    // const client = new MongoClient(uri, {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    //     serverApi: ServerApiVersion.v1,
+    // });
+    // client.connect((err) => {
+    //     const collection = client.db("test").collection("devices");
+    //     // perform actions on the collection object
+    //     if (err) {
+    //         console.log(err)
+    //     } else {
+    //         console.log("connect successfully");
+
+    //     }
+    //     client.close();
+    // });
 }
 module.exports = {
     connect,
